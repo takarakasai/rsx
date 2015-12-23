8f41ed6c4851f01cf57a92d61126149b5e156703
kasai <takara.kasai@gmail.com>
2015-12-24 07:25:00 +0900

    add write mask for virtual memory.

--- h/rsx/mmap/ics3x.h
+++ h/rsx/mmap/ics3x.h
@@ -4,9 +4,6 @@
 
 #include "ics_type.h"
 
-#define ICS30_EEPROM_SIZE 0x1D /* 58 [byte] */
-#define ICS35_EEPROM_SIZE 0x20 /* 64 [byte] */
-
 static const ics_opt_t kICS_VER_MASK = 0x3;
 
 #define ICS_VER_20 0x01 /* not implemented */
@@ -32,7 +29,8 @@ typedef enum {
   ICS30_ROM_SPEED          = 0x12,
 
   ICS30_ROM_OFFSET         = 0x1B,
-  ICS30_ROM_ID             = 0x1C
+  ICS30_ROM_ID             = 0x1C,
+  ICS30_ROM_SIZE           = 0x1D  /* 58/2 [byte] */
 } ICS30_MEM_ADDR;
 
 typedef enum {
@@ -58,16 +56,44 @@ typedef enum {
   ICS35_ROM_ID             = 0x1C,
   ICS35_ROM_C_CHG_STRETCH1 = 0x1D,
   ICS35_ROM_C_CHG_STRETCH2 = 0x1E,
-  ICS35_ROM_C_CHG_STRETCH3 = 0x1F
+  ICS35_ROM_C_CHG_STRETCH3 = 0x1F,
+  ICS35_ROM_SIZE           = 0x20  /* 64/2 [byte] */
 } ICS35_MEM_ADDR;
+
+/* 0x00 is read only address */
+static const uint8_t kIcs30_Mem_Write_Mask[ICS30_ROM_SIZE] = {
+  /*       0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09 */
+  /* 00 */ 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+  /* 10 */ 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 
+  /* 20 */ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF 
+};
+
+/* 0x00 is read only address */
+static const uint8_t kIcs35_Mem_Write_Mask[ICS35_ROM_SIZE] = {
+  /*       0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09 */
+  /* 00 */ 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+  /* 10 */ 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 
+  /* 20 */ 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0xFF,
+  /* 30 */ 0xFF, 0xFF
+};
 
 static inline size_t ics_get_eeprom_size (dps_opt_t option) {
   switch (kICS_VER_MASK & option) {
-    case ICS_VER_30: return ICS30_EEPROM_SIZE;
-    case ICS_VER_35: return ICS35_EEPROM_SIZE;
-    default:         return ICS30_EEPROM_SIZE;
+    case ICS_VER_30: return ICS30_ROM_SIZE;
+    case ICS_VER_35: return ICS35_ROM_SIZE;
+    default:         return ICS30_ROM_SIZE;
   }
   return 0;
+}
+
+static inline errno_t ics_get_eeprom_write_mask (dps_opt_t option, const uint8_t **mask_array) {
+  EVALUE(NULL, mask_array);
+  switch (kICS_VER_MASK & option) {
+    case ICS_VER_30: *mask_array = kIcs30_Mem_Write_Mask; break;
+    case ICS_VER_35: *mask_array = kIcs35_Mem_Write_Mask; break;
+    default:                                              break;
+  }
+  return EOK;
 }
 
 #endif
