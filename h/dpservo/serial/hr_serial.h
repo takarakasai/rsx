c25386f95113a575b981a963c4c8f61608f4d20e
kasai <takara.kasai@gmail.com>
2015-12-25 12:41:38 +0900

    add some convert function for hr_serial.

--- h/dpservo/serial/hr_serial.h
+++ h/dpservo/serial/hr_serial.h
@@ -10,6 +10,7 @@
 #include <termios.h>
 #endif
 
+#include "dp_err.h"
 #include "hr_unixio.h"
 
 #define HR_SERIAL_DECL(name) \
@@ -24,6 +25,25 @@ typedef enum {
   HR_PAR_ODD,
 } hr_parity;
 
+#if 0
+static inline errno_t hr_str2parity (const char* cparity, hr_parity *parity) {
+  EVALUE(NULL, cparity);
+  EVALUE(NULL, parity);
+
+  if (strcmp(cparity, "none") == 0) {
+    *parity = HR_PAR_NONE;
+  } else if (strcmp(cparity, "even")) {
+    *parity = HR_PAR_EVEN;
+  } else if (strcmp(cparity, "odd")) {
+    *parity = HR_PAR_ODD;
+  } else {
+    return EINVAL;
+  }
+
+  return EOK;
+}
+#endif
+
 typedef enum {
   HR_B9600   ,
   HR_B19200  ,
@@ -37,6 +57,27 @@ typedef enum {
   HR_B1152000,
   HR_B1250000
 } hr_baudrate;
+
+static inline errno_t hr_int2baudrate (int32_t baudrate, hr_baudrate *pbaudrate) {
+  EVALUE(NULL, pbaudrate);
+
+  switch (baudrate) {
+    case 9600     : *pbaudrate = HR_B9600   ; break;
+    case 19200    : *pbaudrate = HR_B19200  ; break;
+    case 38400    : *pbaudrate = HR_B38400  ; break;
+    case 57600    : *pbaudrate = HR_B57600  ; break;
+    case 115200   : *pbaudrate = HR_B115200 ; break;
+    case 230400   : *pbaudrate = HR_B230400 ; break;
+    case 460800   : *pbaudrate = HR_B460800 ; break;
+    case 576000   : *pbaudrate = HR_B576000 ; break;
+    case 625000   : *pbaudrate = HR_B625000 ; break;
+    case 1152000  : *pbaudrate = HR_B1152000; break;
+    case 1250000  : *pbaudrate = HR_B1250000; break;
+    default       : return EINVAL ;
+  }
+
+  return EOK;
+}
 
 static inline const char* hr_baudrate2str (hr_baudrate baudrate) {
   switch (baudrate) {
