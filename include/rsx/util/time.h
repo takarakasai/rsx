cfd91937f33e2ea1c319cdc28cb364f1947a97fd
takarakasai <takara.kasai@gmail.com>
2025-01-13 07:06:22 +0900

    support rsx_gettime and rsx_difftime

--- include/rsx/util/time.h
+++ include/rsx/util/time.h
@@ -1,13 +1,24 @@
 #ifndef RSX_UTIL_TIME_H_
 #define RSX_UTIL_TIME_H_
 
+#include "rsx/rsx_err.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void rsx_pause();
+typedef struct {
+  long sec;
+  long nsec;
+} rsx_timespec;
 
-void rsx_wait_usec(long usec);
+errno_t rsx_pause();
+
+errno_t rsx_wait_usec(long usec);
+
+errno_t rsx_gettime(rsx_timespec* timespec);
+
+errno_t rsx_difftime(rsx_timespec* after, rsx_timespec* before, rsx_timespec* result);
 
 #ifdef __cplusplus
 }  //  extern "C"
--- src/util/time.c
+++ src/util/time.c
@@ -1,20 +1,22 @@
-#include <rsx/util/time.h>
+#include "rsx/util/time.h"
 
 #if defined(CYGWIN) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
   #error environment not supported
 #else
+  #include <time.h>
   #include <unistd.h>
   #include <sys/select.h>
 #endif
 
-void rsx_pause() {
+errno_t rsx_pause() {
 #if defined(CYGWIN) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
   pause();
 #else
 #endif
+  return EOK;
 }
 
-void rsx_wait_usec(long usec) {
+errno_t rsx_wait_usec(long usec) {
 #if defined(CYGWIN) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
 #else
   struct timespec time;
@@ -22,4 +24,45 @@ void rsx_wait_usec(long usec) {
   time.tv_nsec = (usec % (1000 * 1000)) * 1000;
   pselect(0, 0, 0, 0, &time, 0);
 #endif
+  return EOK;
+}
+
+errno_t rsx_gettime(rsx_timespec* timespec) {
+  EVALUE(NULL, timespec);
+#if defined(CYGWIN) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#else
+  struct timespec time;
+  int res = clock_gettime(CLOCK_REALTIME, &time);
+  if (res == -1) {
+    return errno;
+  }
+
+  timespec->sec  = time.tv_sec;
+  timespec->nsec = time.tv_nsec;
+
+  return EOK;
+#endif
+}
+
+errno_t rsx_difftime(rsx_timespec* after, rsx_timespec* before, rsx_timespec* result) {
+  EVALUE(NULL, after);
+  EVALUE(NULL, before);
+  EVALUE(NULL, result);
+
+  if (after->sec < before->sec) {
+    return EINVAL;
+  }
+  if (after->sec == before->sec && after->nsec < before->nsec) {
+    return EINVAL;
+  }
+
+  if (after->nsec < before->nsec) {
+    result->sec  = after->sec - before->sec - 1;
+    result->nsec = after->nsec + (1000 * 1000 * 1000 - before->nsec);
+  } else {
+    result->sec  = after->sec - before->sec;
+    result->nsec = after->nsec - before->nsec;
+  }
+
+  return EOK;
 }
